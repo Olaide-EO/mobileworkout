26096951bce5b22dca38e4dfdbd3d72050c3cf94
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-05-27 21:58:06 +0100

    testing folder

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Love God and keep HIS commandment </Text>
+      <Text style={styles.text}>for this is the whole duty of man!</Text>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff'
+  }
 });
+
+export default App
