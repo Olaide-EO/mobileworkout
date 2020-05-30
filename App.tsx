a9d9d6ad8924cfe42a99065e0862d384068278bd
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-05-30 12:45:32 +0100

    add new app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {observer} from 'mobx-react-lite';
+import { CounterStoreContext } from "./store/CounterStore";
+import Router from './router/Router';
 
 const App = () => {
+  const counterStore = useContext(CounterStoreContext);
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Love God and keep HIS commandment </Text>
-      <Text style={styles.text}>for this is the whole duty of man!</Text>
+      <View style={styles.wrapper}>
+       <Router />
+       </View>
+      
     </View>
   );
 }
+
+
+/*
+
+ <Text style={styles.text}>Love God and keep HIS commandment </Text>
+      <Text style={styles.text}>Great!</Text>
+      <Text style={styles.text}>
+                  {counterStore.count}
+              </Text>
+              <Button title="increment" onPress={() => counterStore.count++} />
+
+              */
 
 
 const styles = StyleSheet.create({
@@ -17,10 +35,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    height: "100%",
   },
   text: {
     color: '#fff'
+  },
+  wrapper: {
+    width: '100%',
+    maxWidth: 425,
+
   }
+
 });
 
 export default App
